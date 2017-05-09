85aca5bfbea1bec0f75df53e0a3d2f89039897bf
Tom McClintock <tmcclintock@email.arizona.edu>
2017-05-09 16:05:24 -0700

    Made sure that the get_P.py script works correctly. The power spectra are trust worthy.

--- get_P.py
+++ get_P.py
@@ -13,25 +13,27 @@ Ob = 0.049017
 Om = 0.31834
 Ocdm = Om - Ob
 params = {
-    'output': 'mPk transfer',
-    "h":0.670435,
-    "A_s":2.1e-9,
-    "n_s":0.96191,
-    "omega_b":Ob,
-    "omega_cdm":Ocdm,
-    'YHe':0.24755048455476272,#By hand, default value
-    'P_k_max_1/Mpc':2000,
-    'z_pk':1.1,
-    'non linear':'halofit'}
+        'output': 'mPk',
+        "h":0.670435,
+        "A_s":2.1e-9,
+        "n_s":0.96191,
+        "omega_b":Ob,
+        "omega_cdm":Ocdm,
+        'YHe':0.24755048455476272,#By hand, default value
+        'P_k_max_h/Mpc':2000.,
+        'z_max_pk':1.0,
+        'non linear':'halofit'}
 
 cosmo = Class()
 cosmo.set(params)
 cosmo.compute()
 
+
 k = np.logspace(-5, 3, base=10, num=1000)
 np.savetxt("txt_files/P_files/k.txt", k)
 for i in range(len(zs)):
     z = zs[i]
+
     Pmm  = np.array([cosmo.pk(ki, z) for ki in k])
     Plin = np.array([cosmo.pk_lin(ki, z) for ki in k])
     np.savetxt("txt_files/P_files/Pnl_z%.2f.txt"%(z), Pmm)
