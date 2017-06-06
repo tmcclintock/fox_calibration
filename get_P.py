6b92e14a14db86faa68d5f43bf1ce6dbd9d0fddb
Tom McClintock <tmcclintock@email.arizona.edu>
2017-06-06 08:53:40 -0700

    Clarified the units in get_P.py.

--- get_P.py
+++ get_P.py
@@ -37,6 +37,6 @@ for i in range(len(zs)):
 
     Pmm  = np.array([cosmo.pk(ki, z) for ki in k]) 
     Plin = np.array([cosmo.pk_lin(ki, z) for ki in k]) 
-    np.savetxt("txt_files/P_files/Pnl_z%.2f.txt"%(z), Pmm*h**3) #h^3/Mpc^3
-    np.savetxt("txt_files/P_files/Plin_z%.2f.txt"%(z), Plin*h**3) #h^3/Mpc^3
+    np.savetxt("txt_files/P_files/Pnl_z%.2f.txt"%(z), Pmm*h**3) #Mpc^3/h^3
+    np.savetxt("txt_files/P_files/Plin_z%.2f.txt"%(z), Plin*h**3) #Mpc^3/h^3
     print "Done with z%.2f"%z
