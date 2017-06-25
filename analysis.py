bc4e0d34f7e5fe92e3c736916a3a635a4a1bc2ab
Tom McClintock <tmcclintock89@gmail.com>
2017-06-25 07:49:25 -0700

    Added the MCMC code to analysis.py.

--- analysis.py
+++ analysis.py
@@ -9,6 +9,12 @@ import matplotlib.pyplot as plt
 import emcee
 import scipy.optimize as op
 
+#MCMC parameters
+nwalkers = 4
+ndim = 1
+nsteps = 10000
+nburn = nsteps/4
+
 inds = [6,7,8,9]
 zs = [1.0, 0.5, 0.25, 0.0]
 zstrings = ["1.0", "0.5", "0.25", "0.0"]
@@ -30,6 +36,7 @@ k = np.loadtxt("txt_files/P_files/k.txt")
 
 if __name__ == "__main__":
 
+    """
     for ps in [15, 25, 35]:
         true_lM = np.log10(np.genfromtxt("L_ps%d_masses.txt"%ps))
         bf_masses = np.zeros_like(true_lM)
@@ -62,4 +69,52 @@ if __name__ == "__main__":
             continue #end i,ind
         np.savetxt("output_files/mass_fits/bf_masses_ps%d.txt"%ps, bf_masses)
         np.savetxt("output_files/mass_fits/bf_cal_ps%d.txt"%ps, cal)
+        continue #end ps
+    """
+
+    for ps in [15, 25, 35]:
+        true_lM = np.log10(np.genfromtxt("L_ps%d_masses.txt"%ps))
+        bf_masses = np.loadtxt("output_files/mass_fits/bf_masses_ps%d.txt"%ps)
+        mcmc_masses = np.zeros_like(true_lM)
+        mcmc_stds   = np.zeros_like(mcmc_masses)
+        cal     = np.zeros_like(mcmc_masses)
+        calerr = np.zeros_like(mcmc_masses)
+        for i,ind in zip(range(len(inds)), inds):
+            z = zs[i]
+            Plin = np.loadtxt("txt_files/P_files/Plin_z%.2f.txt"%z)
+            Pmm  = np.loadtxt("txt_files/P_files/Pnl_z%.2f.txt"%z)
+            input_params["R_bin_min"] = 0.0323*(h*(1+z))
+            input_params["R_bin_max"] = 30.0*(h*(1+z))
+            extras = [k, Plin, Pmm, cosmo, input_params]
+            for j in linds:
+                DSpath  = DSdatabase%(ps, i, j)
+                covpath = covdatabase%(ps, i, j)
+                R, DS = np.loadtxt(DSpath).T
+                cov = np.loadtxt(covpath)
+                cut = R>0.2 #Mpc
+                cov = cov[cut]
+                cov = cov[:,cut]
+                icov = np.linalg.inv(cov)
+                DS = DS[cut]
+                R = R[cut]
+                sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(R, DS, icov, cut, z, extras), threads=4)
+                pos = [bf_masses[i, j] + 1e-4*np.random.randn(ndim) for nw in range(nwalkers)]
+                print "Running chain for z=%f l%d starting at lM=%.3f"%(z, j, bf_masses[i,j])
+                sampler.run_mcmc(pos, nsteps)
+                print "\tchain complete for z=%f l%d starting at lM=%.3f"%(z, j, bf_masses[i,j])
+                fullchain = sampler.flatchain
+                likes = sampler.flatlnprobability
+                np.savetxt("output_files/chains/chain_ps%d_z%.2f_l%d.txt"%(z, j), fullchain)
+                np.savetxt("output_files/chains/likes_ps%d_z%.2f_l%d.txt"%(z, j), likes)
+                chain = fullchain[nburn*nwalkers:]
+                mcmc_masses[i,j] = np.mean(chain)
+                mcmc_stds[i,j] = np.std(chain)
+                cal[i,j] = 10**true_lM[i,j]/10**mcmc_masses[i,j]
+                cellerr[i,j] = np.log(10) * mcmc_stds[i,j] * cal[i,j]
+                print "Best fit done for ps%d z%d, l%d"%(ps, ind, j)
+                continue #end j
+            continue #end i,ind
+        np.savetxt("output_files/mass_fits/mcmc_masses_ps%d.txt"%ps, mcmc_masses)
+        np.savetxt("output_files/mass_fits/mcmc_cal_ps%d.txt"%ps, cal)
+        np.savetxt("output_files/mass_fits/mcmc_calerr_ps%d.txt"%ps, calerr)
         continue #end ps
