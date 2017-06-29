ca2d93168a190b043d1196de015fad676439faba
Tom McClintock <tmcclintock@email.arizona.edu>
2017-06-29 11:33:31 -0700

    Working on the calibration. Seems to be some bug only in the MCMC runs.

--- postprocess.py
+++ postprocess.py
@@ -4,14 +4,29 @@ This contains the post-processing steps
 import numpy as np
 import matplotlib.pyplot as plt
 
-calpath = "output_files/mass_fits/bf_cal_ps%d.txt"
+bfpath  = "output_files/mass_fits/bf_cal_ps%d.txt"
+calpath = "output_files/mass_fits/mcmc_cal_ps%d.txt"
+errpath = "output_files/mass_fits/mcmc_calerr_ps%d.txt"
+richpath = "L_ps%d_richnesses.txt"
 
+zs = [1.0, 0.5, 0.25, 0.0]
+fig, axarr = plt.subplots(len(zs), sharex=True, sharey=True)
 if __name__ == "__main__":
-    ps = [15, 25, 35]
-    cal1 = np.loadtxt(calpath%15)
-    cal2 = np.loadtxt(calpath%25)
-    cal3 = np.loadtxt(calpath%35)
+    pss = [15, 25, 35]
 
-    for i in range(len(cal1)):
-        for j in range(len(cal1[i])):
-            print cal1[i,j], cal2[i,j], cal3[i,j]
+    for ps,j in zip(pss,range(len(pss))):
+        bf  = np.loadtxt(bfpath%ps)
+        cal = np.loadtxt(calpath%ps)
+        err = np.loadtxt(errpath%ps)
+        lams = np.genfromtxt(richpath%ps)
+        for i in range(len(zs)):
+            hi = lams[i]>20
+            lo = lams[i]<20
+            z = zs[i]
+            axarr[i].plot(lams[i], bf[i])
+            axarr[i].errorbar(lams[i,hi], cal[i,hi], err[i,hi], marker='o',ls='',c='C%d'%j, alpha=0.5)
+            axarr[i].errorbar(lams[i,lo], cal[i,lo], err[i,lo], marker='o',mfc='white',ls='',c='C%d'%j, alpha=0.5)
+            if j==0:
+                axarr[i].text(60, 1.15, r"$z=%.2f$"%zs[i], fontsize=16)
+
+    plt.show()
